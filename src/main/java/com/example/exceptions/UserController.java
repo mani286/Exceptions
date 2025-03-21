4e80c47df447520056ef317823d8eb60012f5a7c
Potturi <potturim@in.glory-global.com>
2025-03-21 11:41:51 +0530

    secondcommit

--- src/main/java/com/example/exceptions/UserController.java
+++ src/main/java/com/example/exceptions/UserController.java
@@ -1,17 +1,11 @@
 package com.example.exceptions;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.Hidden;
-
 @RestController
-@Hidden
 public class UserController {
 	
 	@Autowired
